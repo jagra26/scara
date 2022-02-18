jacobian fix

scara.py
@@ -8,7 +8,7 @@ l1 = 0.475
 l2 = 0.4
 offset = 0.1
 E = 0.2
-dt = 0.01
+dt = 0.025
 
 # direct kinematics
 
@@ -26,14 +26,14 @@ def extract(T):
   z = T[2][3]
   roll = 0
   pitch = m.pi
-  yaw = m.atan2(-T[1][0], -T[0][0])
+  yaw = m.atan2(T[1][0], T[0][0])
   return np.array([x, y, z, roll, pitch, yaw])
 
 #jacobian matrix
 
 def jacobian(l1, l2, q1, q2):
   return np.array([[-l2*m.sin(q1+q2)-l1*m.sin(q1), -l2*m.sin(q1+q2), 0, 0],
-                  [-l2*m.sin(q1+q2)-l1*m.sin(q1), -l2*m.cos(q1+q2), 0, 0],
+                  [l2*m.cos(q1+q2)+l1*m.cos(q1), l2*m.cos(q1+q2), 0, 0],
                   [0, 0, 0, -1],
                   [0, 0, 0, 0],
                   [0, 0, 0, 0],
@@ -63,33 +63,52 @@ if clientID!=-1:
     joint_pos_2 = (1, 0)
     joint_pos_3 = (1, 0) 
     joint_pos_4 = (1, 0) 
-
+    ret_pos_1 = 1
+    ret_pos_2 = 1
+    ret_pos_3 = 1
+    ret_pos_4 = 1
     # avoid errors
+    while ret_pos_1+ret_pos_2+ret_pos_3+ret_pos_4 != 0 :
+      ret_pos_1 = sim.simxSetJointPosition(clientID, axis_1_handle, -0.2, sim.simx_opmode_oneshot)
+      ret_pos_2 = sim.simxSetJointPosition(clientID, axis_2_handle, 0, sim.simx_opmode_oneshot)
+      ret_pos_3 = sim.simxSetJointPosition(clientID, axis_3_handle, 0, sim.simx_opmode_oneshot)
+      ret_pos_4 = sim.simxSetJointPosition(clientID, axis_4_handle, 0, sim.simx_opmode_oneshot)
+      time.sleep(dt)
+    print(ret_pos_1, ret_pos_2, ret_pos_3, ret_pos_4)
+    time.sleep(.5)
+
 
     while joint_pos_1[0]+joint_pos_2[0]+joint_pos_3[0]+joint_pos_4[0] != 0 :
       joint_pos_1 = sim.simxGetJointPosition(clientID, axis_1_handle, sim.simx_opmode_streaming)
       joint_pos_2 = sim.simxGetJointPosition(clientID, axis_2_handle, sim.simx_opmode_streaming)
       joint_pos_3 = sim.simxGetJointPosition(clientID, axis_3_handle, sim.simx_opmode_streaming)
       joint_pos_4 = sim.simxGetJointPosition(clientID, axis_4_handle, sim.simx_opmode_streaming)
-    
+      time.sleep(dt)
+    print(joint_pos_1, joint_pos_2, joint_pos_3, joint_pos_4)
+    time.sleep(.5)    
       # resolved rate
 
     qm = [joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1]]
     T = fkine(joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1])
     Xd = extract(T)
-    Xm = np.array([0.3188, 0.425, 0.1+offset, 0, m.pi, m.pi/4])
+    Xm = np.array([0.3188, 0.425, 0.1-offset, 0, m.pi, m.pi/4])
+    J = jacobian(l1, l2, joint_pos_1[1], joint_pos_2[1])
+    #print(J)
     while np.linalg.norm(Xd-Xm)>= E:
+      print(np.linalg.norm(Xd-Xm))
       J = jacobian(l1, l2, joint_pos_1[1], joint_pos_2[1])
-      q_dot = np.matmul(np.linalg.pinv(J), Xd-Xm)
+      q_dot = np.dot(np.linalg.pinv(J), Xd-Xm)
       qm = qm + q_dot*dt
 
       # set positions
 
-      ret_pos_1 = sim.simxSetJointPosition(clientID, axis_1_handle, qm[0], sim.simx_opmode_oneshot)
-      ret_pos_2 = sim.simxSetJointPosition(clientID, axis_2_handle, qm[1], sim.simx_opmode_oneshot)
-      ret_pos_3 = sim.simxSetJointPosition(clientID, axis_3_handle, qm[2], sim.simx_opmode_oneshot)
-      ret_pos_4 = sim.simxSetJointPosition(clientID, axis_4_handle, qm[3], sim.simx_opmode_oneshot)
-      print(ret_pos_1, ret_pos_2, ret_pos_3, ret_pos_4)
+      ret_pos_1 = sim.simxSetJointPosition(clientID, axis_1_handle, qm[0], sim.simx_opmode_streaming)
+      ret_pos_2 = sim.simxSetJointPosition(clientID, axis_2_handle, qm[1], sim.simx_opmode_streaming)
+      ret_pos_3 = sim.simxSetJointPosition(clientID, axis_3_handle, qm[2], sim.simx_opmode_streaming)
+      ret_pos_4 = sim.simxSetJointPosition(clientID, axis_4_handle, qm[3], sim.simx_opmode_streaming)
+      time.sleep(dt)
+
+      #print(ret_pos_1, ret_pos_2, ret_pos_3, ret_pos_4)
 
       # get positions
       
@@ -98,7 +117,7 @@ if clientID!=-1:
       joint_pos_3 = sim.simxGetJointPosition(clientID, axis_3_handle, sim.simx_opmode_buffer)
       joint_pos_4 = sim.simxGetJointPosition(clientID, axis_4_handle, sim.simx_opmode_buffer)
       qm = [joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1]]
-      print(qm)
+      #print(qm)
       T = fkine(qm[0], qm[1], qm[2], qm[3])
       Xd = extract(T)
 