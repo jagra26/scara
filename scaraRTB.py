fef43b4bacafcec9555d6dc40c0ff3d26a2eefc5
jagra26 <jagra@ic.ufal.br>
2022-02-21 13:33:41 -0300

    rtb

--- scaraRTB.py
+++ scaraRTB.py
@@ -3,20 +3,25 @@ import math as m
 import time
 import sys
 import sim
+import roboticstoolbox as rtb
+
+# constants
 
 l1 = 0.475
 l2 = 0.4
 offset = 0.1
-E = 0.2
-dt = 0.025
+E = 0.02
+dt = 0.05
 
-# direct kinematics
+#define SCARA
 
-def fkine(theta_1, theta_2, theta_3, d_4):
-  return np.array([[m.cos(theta_3+theta_2+theta_1), m.sin(theta_3+theta_2+theta_1), 0, l1*m.cos(theta_1)+l2*m.cos(theta_2+theta_1)],
-                   [m.sin(theta_3+theta_2+theta_1), -m.cos(theta_3+theta_2+theta_1), 0, l1*m.sin(theta_1)+l2*m.sin(theta_2+theta_1)],
-                   [0, 0, -1, -d_4],
-                   [0, 0, 0, 1]])
+T_0_1 = rtb.robot.DHLink(a = l1, offset=0)
+T_1_2 = rtb.robot.DHLink(a = l2, offset=0)
+T_2_3 = rtb.robot.DHLink(alpha=m.pi, offset=0)
+T_3_4 = rtb.robot.DHLink(sigma=1, qlim=[0, 0.1])
+SCARA = rtb.robot.DHRobot([T_0_1, T_1_2, T_2_3, T_3_4], name="SCARA")
+
+print(SCARA)
 
 # extract x,y,z,roll,pitch,yaw from transformatiom matrix
 
@@ -29,18 +34,6 @@ def extract(T):
   yaw = m.atan2(T[1][0], T[0][0])
   return np.array([x, y, z, roll, pitch, yaw])
 
-#jacobian matrix
-
-def jacobian(l1, l2, q1, q2):
-  return np.array([[-l2*m.sin(q1+q2)-l1*m.sin(q1), -l2*m.sin(q1+q2), 0, 0],
-                  [l2*m.cos(q1+q2)+l1*m.cos(q1), l2*m.cos(q1+q2), 0, 0],
-                  [0, 0, 0, -1],
-                  [0, 0, 0, 0],
-                  [0, 0, 0, 0],
-                  [1, 1, 1, 0]])
-
-def damped_pinv(J, p=0.1):
-  return np.matmul(J.transpose(), np.linalg.inv(np.matmul(J, J.transpose())+(p**2)*np.identity(6)))
 
 print ('Program started')
 sim.simxFinish(-1) # just in case, close all opened connections
@@ -76,15 +69,19 @@ if clientID!=-1:
     ret_pos_3 = 1
     ret_pos_4 = 1
     # avoid errors
+
+    # set initial position
+
     while ret_pos_1+ret_pos_2+ret_pos_3+ret_pos_4 != 0 :
-      ret_pos_1 = sim.simxSetJointPosition(clientID, axis_1_handle, -0.2, sim.simx_opmode_oneshot)
-      ret_pos_2 = sim.simxSetJointPosition(clientID, axis_2_handle, -0.2, sim.simx_opmode_oneshot)
+      ret_pos_1 = sim.simxSetJointPosition(clientID, axis_1_handle, -1, sim.simx_opmode_oneshot)
+      ret_pos_2 = sim.simxSetJointPosition(clientID, axis_2_handle, -1, sim.simx_opmode_oneshot)
       ret_pos_3 = sim.simxSetJointPosition(clientID, axis_3_handle, 0, sim.simx_opmode_oneshot)
       ret_pos_4 = sim.simxSetJointPosition(clientID, axis_4_handle, 0, sim.simx_opmode_oneshot)
       time.sleep(dt)
     print(ret_pos_1, ret_pos_2, ret_pos_3, ret_pos_4)
     time.sleep(.5)
 
+    # set initial position
 
     while joint_pos_1[0]+joint_pos_2[0]+joint_pos_3[0]+joint_pos_4[0] != 0 :
       joint_pos_1 = sim.simxGetJointPosition(clientID, axis_1_handle, sim.simx_opmode_streaming)
@@ -94,17 +91,26 @@ if clientID!=-1:
       time.sleep(dt)
     print(joint_pos_1, joint_pos_2, joint_pos_3, joint_pos_4)
     time.sleep(.5)    
-      # resolved rate
+    
+    # resolved rate
 
     qm = [joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1]]
-    T = fkine(joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1])
-    Xd = extract(T)
-    Xm = np.array([-0.106, -0.6, 0.05-offset, 0, m.pi, -m.pi/2])
-    J = jacobian(l1, l2, joint_pos_1[1], joint_pos_2[1])
-    #print(J)
+    T = SCARA.fkine([joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1]])
+    Xd = extract(T.data[0])
+    a = [0.3188, 0.425, 0.1-offset, 0, m.pi, m.pi/4]
+    b = [-0.106, -0.6, 0.05-offset, 0, m.pi, -m.pi/2]
+    Xm = np.array(a)
+    J = SCARA.jacobe([l1, l2, joint_pos_1[1], joint_pos_2[1]])
+    
+    #Loop
+    
     while np.linalg.norm(Xd-Xm)>= E:
+      # print error
       print(np.linalg.norm(Xd-Xm))
-      J = jacobian(l1, l2, joint_pos_1[1], joint_pos_2[1])
+
+      # calculations
+
+      J = SCARA.jacobe([l1, l2, joint_pos_1[1], joint_pos_2[1]])
       q_dot = np.matmul(np.linalg.pinv(J), (Xd-Xm))
       qm = qm + q_dot*dt
 
@@ -116,8 +122,6 @@ if clientID!=-1:
       ret_pos_4 = sim.simxSetJointPosition(clientID, axis_4_handle, qm[3], sim.simx_opmode_streaming)
       time.sleep(dt)
 
-      #print(ret_pos_1, ret_pos_2, ret_pos_3, ret_pos_4)
-
       # get positions
       
       joint_pos_1 = sim.simxGetJointPosition(clientID, axis_1_handle, sim.simx_opmode_buffer)
@@ -125,9 +129,17 @@ if clientID!=-1:
       joint_pos_3 = sim.simxGetJointPosition(clientID, axis_3_handle, sim.simx_opmode_buffer)
       joint_pos_4 = sim.simxGetJointPosition(clientID, axis_4_handle, sim.simx_opmode_buffer)
       qm = [joint_pos_1[1], joint_pos_2[1], joint_pos_3[1], joint_pos_4[1]]
-      #print(qm)
-      T = fkine(qm[0], qm[1], qm[2], qm[3])
-      Xd = extract(T)
+
+      # calculations
+
+      T = SCARA.fkine(qm)
+      Xd = extract(T.data[0])
+    
+    # end result 
+
+    print("Xd: ", Xd)
+    print("Xm: ", Xm)
+    time.sleep(5)
 
     # stop the simulation:
     sim.simxStopSimulation(clientID,sim.simx_opmode_blocking)
